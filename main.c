ba216a66d112304e7747f348178e9791cb941bf0
Assma <assma.berkanne@gmail.com>
2018-04-04 21:47:30 +0100

    creation des repertoires vide

--- main.c
+++ main.c
@@ -22,3 +22,22 @@ int main(int argc, char *argv[])//argc = la taille de la chaine argv
 		printf("usage: initdev [project-name] [OPTIONS]\nOPTIONAL:\n-[LANG] to specify language (c, cpp, py, latex, beamer supported)\n-[LICENSE] to specify license (GPL, MIT supported)\n-git to initialise a git repository\n");
 		return 0;
 	}
+else{
+		char *projectName;
+		asprintf(&projectName, "%s", argv[1]);
+		//creating new folder with the project's name
+		char *cmd;
+
+		asprintf(&cmd, "mkdir %s", projectName);
+		system(cmd);		
+		
+		//creating empty files
+		//main
+		asprintf(&cmd, "touch %s/%s", projectName, "main");
+		system(cmd);
+		//LICENSE
+		asprintf(&cmd, "touch %s/%s", projectName, "LICENSE");
+		system(cmd);
+		//Makefile
+		asprintf(&cmd, "touch %s/%s", projectName, "Makefile");
+		system(cmd);
